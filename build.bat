e6b585da278debde8f4490931e43bb7823934f83
Vladimir Martynenko <Vladimir@Martynenko.me>
2021-02-13 16:02:34 -0500

    Moved *.java files to /src/main/java and /src/test/java; Updated build scripts to reflect that change; Excluded .DS_Store and build directory from git

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-javac -cp lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar *.java
-java -cp .;lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
-java RunEdgeConvert
+javac -cp build;lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar src/main/java/*.java src/test/java/*.java
+java -cp build;lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
+java -cp build RunEdgeConvert
